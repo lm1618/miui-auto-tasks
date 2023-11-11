328d0f4a614e3706f8b9e5e73b9b63b96c137a8c
lm1618 <63717679+lm1618@users.noreply.github.com>
2023-11-11 13:26:32 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd  /root/miui-auto-tasks
-echo `date` >> test.txt ###向test.txt中插入一条"good morning"
-/usr/bin/python3.8 /root/miui-auto-tasks/miuitask.py
+#cd  /root/miui-auto-tasks
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+pwd
+echo `date` >> test.txt ###向test.txt中插入一条"时间日志"
+/usr/bin/python3 miuitask.py
 r=$(($RANDOM%50)) ###随机生成一个10以内的随机数
 rm -f test.cron                ###删除以前的命令文件
-echo $[r]" 8 * * * /root/miui-auto-tasks/test.sh" >> test.cron #创建并将任务写入cron文件
+echo $[r]" 8 * * * "$( dirname "${BASH_SOURCE[0]}" )"/test.sh" >> test.cron #创建并将任务写入cron文件
 echo "40 3 * * * "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" > /dev/null"   >> test.cron        
-chmod 777  /root/miui-auto-tasks/test.sh  ###给予shell脚本最高执行权限
+chmod 777  test.sh  ###给予shell脚本最高执行权限
 crontab test.cron    ###启动cron任务文件，用于定时自动执行
